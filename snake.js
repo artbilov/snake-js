2e0268a9497714c5ac71811672b3dbe7080b43bf
Archy_k <artbilov@gmail.com>
2024-02-02 16:38:04 -0800

    add apple

--- snake.js
+++ snake.js
@@ -15,31 +15,55 @@ const apple = [
 render()
 
 setTimeout(() => {
-  snake.push({x: 5, y: 1})
+  snake.push({ x: 5, y: 1 })
   snake.shift()
 
   render()
 }, 1000)
 
 setTimeout(() => {
-  snake.push({x: 6, y: 1})
+  snake.push({ x: 6, y: 1 })
   snake.shift()
 
   render()
 }, 2000)
 
 setTimeout(() => {
-  snake.push({x: 7, y: 1})
-
+  snake.push({ x: 7, y: 1 })
+  apple.shift()
   render()
 }, 3000)
 
+setTimeout(() => {
+  snake.push({ x: 8, y: 1 })
+  snake.shift()
+
+  render()
+}, 4000)
+
+setTimeout(() => {
+  snake.push({ x: 9, y: 1 })
+  snake.shift()
+
+  render()
+}, 5000)
+
+
+
+setInterval( () => {
+   
+  snake.push(targetToPush)
+  snake.shift()
+
+  render()
+}, 1000, direction)
 
 
 
 
 function render() {
   clearBoard()
+  addApple()
   renderApple()
   renderSnake()
 }
@@ -49,6 +73,15 @@ function clearBoard() {
   cells.forEach(cell => {
     cell.removeAttribute('class')
   });
+}
+
+function addApple() {
+  if (apple.length > 0) return
+  let x = Math.floor(Math.random() * 20)
+  let y = Math.floor(Math.random() * 20)
+
+  if (snake.some(cell => cell.x == x && cell.y == y)) addApple()
+  else apple.push({ x, y })
 }
 
 function renderSnake() {
